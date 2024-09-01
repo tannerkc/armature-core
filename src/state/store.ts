48a00c2d029090f35ae88e11deda7ed41cff3448
Tanner Cottle <tanner@laconception.io>
2024-09-01 09:00:03 -0400

    wip: enhanced store | added middleware support

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,118 +1,53 @@
-import { useEffect, useState } from ".";
-import { generateUniqueId } from "../utils/generateId";
+import { useState } from ".";
 
-type Listener = () => void;
-type Middleware<T> = (store: Store<T>) => (next: (action: Action) => void) => (action: Action) => void;
-type Selector<T, R> = (state: T) => R;
-
-interface Action {
-  type: string;
-  payload?: any;
-}
-
-class Store<T extends object> {
-  private state: T;
-  private listeners: Set<Listener> = new Set();
-  private middleware: Middleware<T>[] = [];
-
-  constructor(initialState: T, private name: string) {
-    this.state = this.deepFreeze({ ...initialState });
-  }
-
-  getState(): Readonly<T> {
-    return this.state;
-  }
-
-  dispatch = (action: Action): void => {
-    const composedMiddleware = this.composeMiddleware();
-    composedMiddleware(action);
-  };
-
-  private setStateInternal = (newState: Partial<T>): void => {
-    this.state = this.deepFreeze({ ...this.state, ...newState });
-    this.notifyListeners();
-  };
-
-  subscribe(listener: Listener): () => void {
-    this.listeners.add(listener);
-    return () => this.listeners.delete(listener);
-  }
-
-  private notifyListeners(): void {
-    this.listeners.forEach((listener) => listener());
-  }
-
-  use(middleware: Middleware<T>): void {
-    this.middleware.push(middleware);
-  }
-
-  private composeMiddleware() {
-    return this.middleware.reduceRight(
-      (composed, m) => m(this)(composed),
-      (action: Action) => this.reducer(action)
-    );
-  }
-
-  private reducer = (action: Action): void => {
-    switch (action.type) {
-      case `${this.name}/setState`:
-        this.setStateInternal(action.payload);
-        break;
-      default:
-        break;
-    }
-  };
-
-  private deepFreeze<O>(obj: O): Readonly<O> {
-    Object.freeze(obj);
-    Object.getOwnPropertyNames(obj).forEach((prop) => {
-      if (
-        obj[prop] !== null &&
-        (typeof obj[prop] === "object" || typeof obj[prop] === "function") &&
-        !Object.isFrozen(obj[prop])
-      ) {
-        this.deepFreeze(obj[prop]);
-      }
-    });
-    return obj;
-  }
-}
-
-export function createStore<T extends object>(initialState: T, name: string): Store<T> {
-  return new Store<T>(initialState, name);
-}
-
-export function useStore<T extends object, R>(
-  store: Store<T>,
-  selector: Selector<T, R>
-): [R, (action: Action) => void] {
-  const [, forceUpdate] = useState(0);
-
-  useEffect(() => {
-    const unsubscribe = store.subscribe(() => forceUpdate((v) => v + 1));
-    return unsubscribe;
-  }, [store]);
-
-  return [selector(store.getState()), store.dispatch];
-}
-
-export const loggerMiddleware: Middleware<any> = (store) => (next) => (action) => {
-  console.log('Dispatching:', action);
-  next(action);
-  console.log('New State:', store.getState());
+type Store<T> = {
+    [K in keyof T]: [() => T[K], (newValue: T[K]) => void];
 };
 
-export const thunkMiddleware: Middleware<any> = (store) => (next) => (action) => {
-  if (typeof action === 'function') {
-    return action(store.dispatch, store.getState);
-  }
-  return next(action);
-};
+type Middleware<T> = (store: Store<T>, key: keyof T, action: (newValue: T[keyof T]) => void) => (newValue: T[keyof T]) => void;
 
-export function createAction<P = void, T extends string = string>(
-  type: T
-): (...args: P extends void ? [] : [P]) => Action {
-  return (payload?: P) => ({ type, payload });
+const stores: Record<string, Store<any>> = {};
+const globalMiddlewares: Middleware<any>[] = [];
+
+export const use = (middleware: Middleware<any>) => {
+  globalMiddlewares.push(middleware);
 }
 
-export { useState, useEffect };
+export const createStore = <T extends object>(
+  initialState: T, 
+  storeId: string, 
+  storeSpecificMiddleware: Middleware<T>[] = []
+): Store<T> => {
+  if (stores[storeId]) {
+    throw new Error(`Store with id '${storeId}' already exists`);
+  }
+
+  const store: Partial<Store<T>> = {};
+  const allMiddlewares = [...globalMiddlewares, ...storeSpecificMiddleware];
+
+  for (const key in initialState) {
+    const [getter, setter] = useState(initialState[key], `${storeId}_${key}`);
+    
+    const enhancedSetter = (newValue: T[keyof T]) => {
+      let dispatch = setter;
+      allMiddlewares.forEach(mw => {
+        dispatch = mw(store as Store<T>, key, dispatch as any);
+      });
+      dispatch(newValue as any);
+    };
+
+    store[key] = [getter, enhancedSetter];
+  }
+
+  stores[storeId] = store as Store<T>;
+  return store as Store<T>;
+}
+
+export const useStore = <T, K extends keyof T>(store: Store<T>, key: K): [() => T[K], (newValue: T[K]) => void] => {
+  return store[key];
+}
+
+export const loggerMiddleware: Middleware<any> = (store, key, next) => (newValue) => {
+  console.log(`Key '${key.toString()}' updated with value:`, newValue);
+  next(newValue);
+};
